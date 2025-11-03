1e0ee3b761a2f0a92fae9b174b2cd35b997026c0
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-11-03 10:24:35 +0600

    pricing done

--- src/pages/home/Pricing.jsx
+++ src/pages/home/Pricing.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import HeadingSection from "../../components/HeadingSection";
+import { list } from "postcss";
 
 const pricingPlans = [
   {
@@ -46,13 +47,15 @@ const pricingPlans = [
 const Pricing = () => {
   const [isYearly, setIsYearly] = useState(false);
   return (
-    <div className="container mx-auto max-w-screen-2xl">
+    <div className="max-wscreen-2xl container mx-auto py-20 px-5">
       <HeadingSection
+        heading="Choose Your Plan"
         subheading="Pricing"
-        heading="Choose Your Plan
-"
-        description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+        description={
+          "Our pricing plans are flexible and designed to fit your needs. Choose a plan that works best for you."
+        }
       />
+
       {/* toggle for yearly/monthly plan */}
 
       <div className="text-center mb-10">
@@ -75,6 +78,67 @@ const Pricing = () => {
             ></div>
           </div>
         </label>
+      </div>
+
+      {/* princing plan */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+        {pricingPlans.map((plan) => (
+          <div
+            key={plan.name}
+            className="bg-white border border-gray-200 rounded-lg overflow-hidden transition-all duration-300 hover:shadow-xl"
+          >
+            {/* top */}
+            <div className="p-5 text-center">
+              <h3 className="text-2xl font-semibold text-gray-800 my-3">
+                {plan.name}
+              </h3>
+              <p className="text-gray-500 mb-5">
+                Best for {plan.name.toLowerCase()}s
+              </p>
+
+              {/* pricing */}
+              <div className="text-3xl font-bold text-gray-800">
+                ${isYearly ? plan.yearlyPrice : plan.monthlyPrice}
+                <span className="text-sm text-gray-500">
+                  /{isYearly ? "year" : "month"}
+                </span>
+              </div>
+              <p className="mt-5"> {isYearly ? plan.saves : ""}</p>
+            </div>
+
+            {/* button */}
+            <div className="p-5 text-center">
+              <button className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition duration-300">
+                Choose {plan.name}
+              </button>
+            </div>
+
+            {/* features */}
+            <div className="p-5 md:px-12 md:py-5">
+              <ul className="text-center text-gray-600">
+                {plan.features.map((feature, index) => (
+                  <li key={index} className="flex items-center mb-2">
+                    <svg
+                      className="w-4 h-4 text-blue-600 mr-2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M5 12l5 5L20 7"
+                      />
+                    </svg>{" "}
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
